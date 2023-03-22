971d52882ee530b4652b7985f715b2253dcd12d5
abhijitnr <abhijitbiswas8859@gmail.com>
2023-03-22 13:53:07 +0530

    Auto suggestion in react added

---
+++ react/auto_suggestion/README.md
@@ -0,0 +1,1 @@
+# React Auto Suggestion
---
+++ react/auto_suggestion/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+import Search from "./components/Search";
+import Suggestions from "./components/Suggestions";
+
+function App() {
+  return (
+    <div className="App">
+      <Search />
+      <Suggestions />
+    </div>
+  );
+}
+
+export default App;
